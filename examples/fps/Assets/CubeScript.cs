a417cd2a7028f8152a86dc470c57b0688a974254
Mike Treanor <treanor@american.edu>
2024-01-26 12:19:06 -0500

    added some comments

--- examples/fps/Assets/CubeScript.cs
+++ examples/fps/Assets/CubeScript.cs
@@ -17,18 +17,30 @@ public class CubeScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            // This moves the cube forward toward the "Moon" (sphere). This is an example
+            // of how to move things forward.
             gameObject.transform.position = gameObject.transform.position + gameObject.transform.forward;
         }
     }
 
+    // Unity will tell the function below to run under the following conditions:
+    //  1. Two objects with colliders are colliding
+    //  2. At least one of the objects' colliders are marked as "Is Trigger"
+    //  3. At least one of the objects has a Rigidbody
     private void OnTriggerEnter(Collider other)
     {
+        // 'other' is the name of the collider that just collided with the object
+        // that this script ("CubeScript") is attached to. So the if statment
+        // below checks to see that that object has the tag "moon". Remember that
+        // the tags for GameObjects are assigned in the top left area of the
+        // inspector when you select the object inside of Unity.
         if (other.CompareTag("moon"))
         {
             Debug.Log(other.gameObject.name);
             Renderer rend = gameObject.GetComponent<Renderer>();
             rend.material.color = Color.green;
 
+            // Instantiate 200 confetto, thus creating confetti.
             for (int i = 0; i < 200; i++)
             {
                 GameObject confetto = Instantiate(confettoPrefab, gameObject.transform.position, Quaternion.identity);
